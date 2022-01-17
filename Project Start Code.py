72b73795e7b7f9f7619ae4f5063dfab037d57e50
RhornRobinson <97114557+RhornRobinson@users.noreply.github.com>
2022-01-17 12:26:57 +0000

    Add files via upload
    This is a draft of new project. Will amend and comment daily.

---
+++ Project Start Code.py
@@ -0,0 +1,16 @@
+
+from time import sleep
+import colorsys
+
+
+
+leds = [(9255,255,255)]*360   # list of 360 tuples, each containing R,G,B values.
+
+value = input ( ''' Welcome to The Car Showroom. Pick a car option below: 
+                \t Type the number of your choice and press Enter.
+                \t 1. Mercedes
+                \t 2. Lexus
+                \t 3. Chevrolet
+                 ''')
+
+
